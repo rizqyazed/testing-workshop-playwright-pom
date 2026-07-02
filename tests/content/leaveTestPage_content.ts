3742064b651ee1b1e4b34609d0708bd4fc071b44
rizq yazed <rizqyazed@gmail.com>
2026-07-02 13:29:30 +0100

    added pages

---
+++ tests/content/leaveTestPage_content.ts
@@ -0,0 +1,15 @@
+const leaveTestPage_content = {
+  pageTitle:
+    "Is the holiday entitlement based on: - Calculate holiday entitlement - GOV.UK",
+  heading: "Is the holiday entitlement based on:",
+  label1: "days worked per week",
+  label2: "hours worked per week",
+  label3: "annualised hours",
+  label4: "compressed hours",
+  label5: "compressed hours",
+  continue_button: "Continue",
+  error_message: "Please answer this question",
+  error_heading: "There is a problem",
+} as const;
+
+export default leaveTestPage_content;
